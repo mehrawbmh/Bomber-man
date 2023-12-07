b84708a1fcf2f4e3d27b26618fdaf0fcb423d75b
mehrab <mehrabmh81@yahoo.com>
2023-12-08 01:29:35 +0330

    just some cleaning and define a const size for elements

--- headers/map_element.hpp
+++ headers/map_element.hpp
@@ -5,14 +5,21 @@
 
 
 enum MapElementTypes {GRASS = 0, BREAKABLE_WALL, UNBREAKABLE_WALL, NROFTYPES};
+
 class MapElement {
 public:
+    const sf::Vector2f DEFAULT_SPRITE_SIZE = sf::Vector2f (100.f, 100.f);
+
     MapElement(const sf::Vector2f &position);
 
     void setPosition(const sf::Vector2f &newPosition);
+
     int type;
+
     virtual void draw(sf::RenderTarget *window) = 0;
+
     const sf::Sprite getSprite() const;
+
     sf::Vector2f getPosition() const;
 
 protected:
